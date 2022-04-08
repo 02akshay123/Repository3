a1eed5eb3dd35de5295899dc6bcc7c00913978de
02akshay123 <akshaymjadhav96@gmail.com>
2022-04-09 01:57:12 +0530

    Third
    Third commit

---
+++ Dec21Evening1/src/groupD/Rollnumber.java
@@ -0,0 +1,12 @@
+package groupD;
+
+public class Rollnumber {
+	public static void main(String[] args) {
+		System.out.println("11");
+		System.out.println("21");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+	}
+
+}
